2afbb1d34dcaac547ce042aa2cc5e58b0748ba0c
mxjwkv <mxjwkv@gmail.com>
2015-08-28 16:33:23 -0700

    Change libfunc lib name

--- build_libfunc.bat
+++ build_libfunc.bat
@@ -1,7 +1,7 @@
 ::build_libfunc.bat
 ::-----------------------
 ::    Created: 26-AUG-2015
-::Last Update: 26-AUG-2015
+::Last Update: 27-AUG-2015
 ::
 ::WINDOWS
 @echo off
@@ -10,14 +10,13 @@
 echo --- Release ------------------------------------------
 ..\..\__incrbuildnum __build_num_release -L
 cl *.cpp /O2 /MT /c /EHsc /W3
-lib *.obj /OUT:..\lib\libfunc.lib
-lib /LIST:..\lib\libfunc.txt ..\lib\libfunc.lib
-echo libfunc.lib contains:
-type ..\lib\libfunc.txt
+lib *.obj /OUT:..\lib\wlp_libfunc.lib
+lib /LIST:..\lib\wlp_libfunc.txt ..\lib\wlp_libfunc.lib
+echo wlp_libfunc.lib contains:
+type ..\lib\wlp_libfunc.txt
 :: -------------------------------------
 :: Build DEBUG Lib - Multi-Threaded
 echo --- Debug --------------------------------------------
 ..\..\__incrbuildnum __build_num_debug -L
 cl *.cpp /D "_DEBUG" /MTd /O2 /c /EHsc /W3
-:::lib *.obj /OUT:..\libfunc_win32D.lib
-lib *.obj /OUT:..\lib\libfuncD.lib
+lib *.obj /OUT:..\lib\wlp_libfuncD.lib
